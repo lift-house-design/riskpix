2e22afe9a279e478ad7ebb6ba90523339df1ffa5
Nick Niebaum <nick@nickniebaum.com>
2014-09-26 12:20:52 -0400

    Testing new form styles

--- application/views/signup/index.php
+++ application/views/signup/index.php
@@ -6,45 +6,75 @@
         <div class="contact">
             <div class="center540">
                 <h2 class="oleo c0">sign up - step 1 of 4</h2>
-                <!--form id="contact-form" method="post">
-                <input id="form-check" type="hidden" name="00<?= sha1(rand(0,time())) ?>" value=""/>
-                <table border="0" cellpadding="5" cellspacing="0">
-                <tr>
-                <td class="align-right">Company Name:</td>
-                <td><input type="text" name="company" placeholder="Company Name" value="<?= set_value('company'); ?>"/></td>
-                </tr>
-                <tr>
-                <td class="align-right">First &amp; Last Name:</td>
-                <td><input type="text" name="name" placeholder="Your Name" value="<?= set_value('name'); ?>"/></td>
-                </tr>
-                <tr>
-                <td class="align-right">Enter your email address:</td>
-                <td><input type="text" name="email" placeholder="Email" value="<?= set_value('email'); ?>"/></td>
-                </tr>
-                <tr>
-                <td class="align-right">Re-enter your email:</td>
-                <td><input type="text" name="email2" placeholder="Re-enter Email" value="<?= set_value('email2'); ?>"/></td>
-                </tr>
-                <tr>
-                <td class="align-right">Eight character password:</td>
-                <td><input type="password" name="password" placeholder="Password" value="<?= set_value('password'); ?>"/></td>
-                </tr>
-                <tr>
-                <td class="align-right">Re-enter your password:</td>
-                <td><input type="password" name="password2" placeholder="Re-enter Password" value="<?= set_value('password2'); ?>"/></td>
-                </tr>
-                </table>
-                <input type="submit" value="Procede to Step 2"/>
-                </form-->
                 <?php echo form_open() ?>
                 <div class="form-field">
-                    <label for="field_company">Company Name</label>
+                    <?php echo form_label('Company Name','field_company') ?>
                     <div class="field">
                         <?php echo form_input(array(
                             'id'=>'field_company',
                             'name'=>'company',
+                            'placeholder'=>'Company Name',
+                            'value'=>set_value('company'),
                         )) ?>
                     </div>
+                </div>
+                <div class="form-field">
+                    <?php echo form_label('First &amp; Last Name','field_name') ?>
+                    <div class="field">
+                        <?php echo form_input(array(
+                            'id'=>'field_name',
+                            'name'=>'name',
+                            'placeholder'=>'Your Name',
+                            'value'=>set_value('name'),
+                        )) ?>
+                    </div>
+                </div>
+                <div class="form-field">
+                    <?php echo form_label('Enter your email address','field_email') ?>
+                    <div class="field">
+                        <?php echo form_input(array(
+                            'id'=>'field_email',
+                            'name'=>'email',
+                            'placeholder'=>'Email',
+                            'value'=>set_value('email'),
+                        )) ?>
+                    </div>
+                </div>
+                <div class="form-field">
+                    <?php echo form_label('Re-enter your email','field_email2') ?>
+                    <div class="field">
+                        <?php echo form_input(array(
+                            'id'=>'field_email2',
+                            'name'=>'email2',
+                            'placeholder'=>'Re-enter Email',
+                            'value'=>set_value('email2'),
+                        )) ?>
+                    </div>
+                </div>
+                <div class="form-field">
+                    <?php echo form_label('Eight character password','field_password') ?>
+                    <div class="field">
+                        <?php echo form_input(array(
+                            'id'=>'field_password',
+                            'name'=>'password',
+                            'placeholder'=>'Password',
+                            'value'=>set_value('password'),
+                        )) ?>
+                    </div>
+                </div>
+                <div class="form-field">
+                    <?php echo form_label('Re-enter your password','field_password2') ?>
+                    <div class="field">
+                        <?php echo form_input(array(
+                            'id'=>'field_password2',
+                            'name'=>'password2',
+                            'placeholder'=>'Re-enter Password',
+                            'value'=>set_value('password2'),
+                        )) ?>
+                    </div>
+                </div>
+                <div class="form-buttons">
+                    <?php echo form_submit(FALSE,'Proceed to Step 2') ?>
                 </div>
                 <?php echo form_close() ?>
             </div>
